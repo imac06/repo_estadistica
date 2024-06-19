ce95165b451a71a12255b922935286ea78efd45d
Galilea Alonso Hernnadez <230110012@itsoeh.edu.mx>
2024-06-19 11:41:48 -0600

    Solucion agregada

---
+++ .ipynb_checkpoints/Galileatest-checkpoint.py
@@ -0,0 +1,6 @@
+import CodeBase.Galilea as gal
+
+clases_sorted = [1, 2, 3, 4, 5]
+fabs_sorted = [5, 6, 7, 7, 5]
+fre = gal.frecuencia_relativa(clases_sorted, fa_sorted)
+print(fre)  
---
+++ Galileatest.py
@@ -0,0 +1,6 @@
+import CodeBase.Galilea as gal
+
+clases_sorted = [1, 2, 3, 4, 5]
+fabs_sorted = [5, 6, 7, 7, 5]
+fre = gal.frecuencia_relativa(clases_sorted, fabs_sorted)
+print(fre)
